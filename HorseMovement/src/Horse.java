52408bf8490f10c690709c195441dce923fac998
Ignacio Estevo <ignacio.estevo@ing.austral.edu.ar>
2021-04-14 12:36:14 -0300

    Update Horse.java

--- HorseMovement/src/Horse.java
+++ HorseMovement/src/Horse.java
@@ -31,7 +31,7 @@ int maxMoves = 4;
         possiblePositions = new Position[8];
         DinamicStack<Position> possibleMoves = new DinamicStack<>();
 
-        possiblePositions[0] = new Position(this.position.getColumn(), this.position.getColumn() +1);
+        possiblePositions[0] = new Position(this.position.getColumn() -2, this.position.getRow() +1);
         possiblePositions[1] = new Position(this.position.getColumn() -2, this.position.getRow() -1);
         possiblePositions[2] = new Position(this.position.getColumn() -1, this.position.getRow() +2);
         possiblePositions[3] = new Position(this.position.getColumn() +1, this.position.getRow() -2);
